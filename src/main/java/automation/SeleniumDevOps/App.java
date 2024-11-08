91c82cf5230d1fee26b3edc970e3da5845020b5b
Vinoth <Vinoth@VinothR>
2024-11-08 09:44:21 +0530

    Entire project

---
+++ src/main/java/automation/SeleniumDevOps/App.java
@@ -0,0 +1,10 @@
+package automation.SeleniumDevOps;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
